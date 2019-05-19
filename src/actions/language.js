27d1eadd1838e9997a9e4feac784b0f5e546c485
ibetadmin <42631185+ibetadmin@users.noreply.github.com>
2019-05-19 10:45:38 -0700

    Update language.js

--- src/actions/language.js
+++ src/actions/language.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { config } from "../util_config";
-const API_URL = process.env.REACT_APP_REST_API;
+//const API_URL = process.env.REACT_APP_REST_API;
+const API_URL = 'http://52.9.147.67:8080/';
 
 const setLanguageState = (language) => {
     return {
