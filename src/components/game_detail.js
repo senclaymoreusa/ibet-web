03f0af9e41a06c9e0de8599e4cd460d3b7f87471
ibetadmin <42631185+ibetadmin@users.noreply.github.com>
2019-05-19 10:44:51 -0700

    Update game_detail.js

--- src/components/game_detail.js
+++ src/components/game_detail.js
@@ -5,8 +5,8 @@ import axios from 'axios';
 import { config } from '../util_config';
 import { FormattedMessage } from 'react-intl';
 
-const API_URL = process.env.REACT_APP_REST_API;
-
+//const API_URL = process.env.REACT_APP_REST_API;
+const API_URL = 'http://52.9.147.67:8080/';
 
 class Game_Detail extends Component {
   
