76e9ae7a404fb4e2177379284cfc2f3a1eb0352a
Samet <samet@claymoreusa.com>
2019-05-21 15:26:20 -0700

    splitted top navbar

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+ var webpack = require('webpack');
+     
+ module.exports = {
+     entry: './js/app.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'app.bundle.js'
+     },
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: "file-loader?name=assets/[name].[ext]",
+                 //include: [Path.join(__dirname, "src/assets")],
+                 query: {
+                     presets: ['es2015', 'react']
+                 }
+             }
+         ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
