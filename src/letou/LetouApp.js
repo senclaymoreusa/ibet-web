cd1df2e78b61c01d15d29bcd12b079b389a9c761
Jennie Hu <jiaqi@claymoreusa.com>
2020-02-03 11:46:14 -0800

    language

--- src/letou/LetouApp.js
+++ src/letou/LetouApp.js
@@ -6,9 +6,11 @@ import { IntlProvider } from 'react-intl';
 import { messages } from './components/messages';
 import { setLanguage } from '../actions/language';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-
+import axios from 'axios';
 
 import './css/global.css';
+
+const API_URL = process.env.REACT_APP_DEVELOP_API_URL;
 
 class LetouApp extends Component {
     constructor(props) {
@@ -21,7 +23,47 @@ class LetouApp extends Component {
 
     componentDidMount() {
         //this.props.getLanguage();
-        this.props.setLanguage("th")
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "config.js"
+        document.body.appendChild(script);
+        
+        const script2 = document.createElement("script");
+        script2.type = "text/javascript";
+        script2.src = "iovation.js"
+        document.body.appendChild(script2);
+
+        var bbData = window.IGLOO.getBlackbox();
+        var language = 'en';
+        if (bbData.finished) {
+            // clearTimeout(timeoutId);
+            var blackBoxString = bbData.blackbox;
+            axios
+                .get(
+                    API_URL + 'users/api/login-device-info?bb=' + blackBoxString
+                )
+                .then(res => {
+                    try{
+                        var location = res.data.details.realIp.ipLocation.countryCode
+                        console.log(location)
+                        if (location === 'CN') {
+                            language = 'zh';
+                        } else if (location === 'TH') {
+                            language = 'th';
+                        } else if (location === 'VN') {
+                            language = 'vi';
+                        } else {
+                            language = 'en';
+                        }
+
+                    }
+                    catch{
+                        language = 'en';
+                    }
+
+                })
+        }
+        this.props.setLanguage(language)
 
     }
 
