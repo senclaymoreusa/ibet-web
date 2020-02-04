0b37f432514e8faf2292104fa6485874b3ab0f1d
Kevin Chang <kevin@claymoreusa.com>
2020-02-04 14:47:46 -0800

    added routing for vn rebate bonuses

---
+++ src/letou/components/bonus/vn_special_lottery.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { images } from '../../../util_config';
+
+const styles = {
+    offerHeader: {
+        height: 55,
+    },
+    mainLogo: {
+        height: 33,
+        paddingLeft: 250,
+        marginTop: 11,
+    },
+    button: {
+        width: 95,
+        height: 30,
+        backgroundColor: '#FF9E00',
+        borderRadius: 5,
+        float: 'right',
+        marginRight: 250,
+        marginTop: 13,
+        '&:hover': {
+            backgroundColor: '#ff8a00'
+        },
+        color: 'white',
+        fontSize: 14,
+        textAlign: 'center',
+        lineHeight: 2.0,
+    },
+    footer: {
+        backgroundColor: '#f5f5f5',
+        borderTop: '1px solid #ddd',
+        fontSize: 12,
+        textAlign: 'center',
+        paddingTop: 20,
+        paddingBottom: 30,
+    },
+};
+
+
+class VNSpecialLotteryBonus extends React.Component {
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <div className={classes.offerHeader}>
+                    <img className={classes.mainLogo} src={ images.src + "letou/logo_vi.png" } alt="Main Logo"/>
+                    <div className={classes.button}>
+                        <a href="/">Xem thêm</a>
+                    </div>
+                </div>
+
+                <div className={classes.content}>
+                </div>
+
+                <div className={classes.footer}>
+                    Letou.com được cấp phép bởi Pagcor Philippines Offshore Gaming.
+                    <br/>
+                    Bản quyền thuộc về LETOU.
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(VNSpecialLotteryBonus);
--- src/letou/letou_routes.js
+++ src/letou/letou_routes.js
@@ -179,6 +179,12 @@ import VNFirstTime100LiveCasinoBonus from './components/bonus/vn_first_time_100_
 import VNFirstTime200ScratchCardBonus from './components/bonus/vn_first_time_200_scratch_card';
 import VNFirstTime200FGOBonus from './components/bonus/vn_first_time_200_fgo';
 
+import VNSpecialEsportBonus from './components/bonus/vn_special_esport';
+import VNSpecialSportsBookBonus from './components/bonus/vn_special_sports_book';
+import VNSpecialKenoBonus from './components/bonus/vn_special_keno';
+import VNSpecialCasinoBonus from './components/bonus/vn_special_casino';
+import VNSpecialSlotBonus from './components/bonus/vn_special_slot';
+import VNSpecialLotteryBonus from './components/bonus/vn_special_lottery';
 
 const containerStyle = {
     minHeight: '100%'
@@ -368,6 +374,13 @@ const BaseRouter = () => (
         <Route exact path="/vi/promotion/first_time_100live_casion" component={VNFirstTime100LiveCasinoBonus} />
         <Route exact path="/vi/promotion/first_time_200scratchcard" component={VNFirstTime200ScratchCardBonus} />
         <Route exact path="/vi/promotion/first_time_200FGo" component={VNFirstTime200FGOBonus} />
+
+        <Route exact path="/vi/promotion/special_esport" component={VNSpecialEsportBonus} />
+        <Route exact path="/vi/promotion/special_sports_book" component={VNSpecialSportsBookBonus} />
+        <Route exact path="/vi/promotion/special_keno" component={VNSpecialKenoBonus} />
+        <Route exact path="/vi/promotion/speicial_casino" component={VNSpecialCasinoBonus} />
+        <Route exact path="/vi/promotion/special_slot" component={VNSpecialSlotBonus} />
+        <Route exact path="/vi/promotion/special_lottery" component={VNSpecialLotteryBonus} />
 
         <Route exact path="/p/:type?/:sub?/:operation?" component={Profile} />
         <Route exact path="/promotions" component={Promotion} />
