60d40e9577c8bbc69646b4add214a450308963f0
Kevin Chang <kevin@claymoreusa.com>
2020-02-14 13:54:55 -0800

    one click & deposit auto free

--- src/letou/components/bonus/th_deposit_auto_free.js
+++ src/letou/components/bonus/th_deposit_auto_free.js
@@ -44,16 +44,56 @@ const styles = {
         textAlign: 'center',
         paddingBottom: 15,
         fontWeight: 900,
-    },
-    content: {
-        margin: 15,
         width: 1000,
         display: 'block',
         marginLeft: 'auto',
         marginRight: 'auto',
-        paddingBottom: 50,
+    },
+    content: {
+        marginTop: 15,
         lineHeight: 2,
         color: 'grey',
+    },
+    mainInfo: {
+        borderTop: '1px solid #ddd',
+        backgroundColor: '#FFFCFA',
+        paddingTop: 30,
+        paddingBottom: 30,
+    },
+    mainInfoText: {
+        width: 1000,
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    wrapper: {
+        fontWeight: 900,
+        borderLeft: '5px solid #FF9E00',
+        paddingLeft: 10,
+        fontSize: 22,
+        marginBottom: 15,
+    },
+    tableHeader: {
+        backgroundColor: 'orange',
+        color: 'white',
+    },
+    table: {
+        borderSpacing: 0,
+        marginTop: 25,
+        marginBottom: 25,
+    },
+    tableCell: {
+        border: '1px solid grey',
+        width: 350,
+        textAlign: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 6,
+        paddingBottom: 6,
+    },
+    contentHeading: {
+        fontWeight: 900,
+        fontSize: 22,
     },
 };
 
@@ -76,8 +116,54 @@ class THDepositAutoFreeBonus extends React.Component {
                 <div className={classes.content}>
                     
                     <h3 className={classes.mainHeading}>
+                        ฝากเงินผ่าน Help2Pay รับโบนัสเพิ่มทันที 5%
+                        สูงสุดถึง 300 บาท!
                     </h3>
 
+                    <div className={classes.mainInfo}>
+                        <div className={classes.mainInfoText}>
+                            <div className={classes.wrapper}>
+                                ระยะเวลาและเงื่อนไข
+                            </div>
+
+                            <ul>
+                                <li>1. ระยะเวลาโปรโมชั่นตั้งแต่ 1 ~ 29 ก.พ. 63 23:59:59 (GMT + 8) เฉพาะบัญชีที่ลงทะเบียนกับ Letou ด้วย THB เท่านั้น</li>
+                                <li>2. ในช่วงระยะเวลาโปรโมชั่นสมาชิกจะต้องทำการฝากเงินผ่านระบบ Help 2 Pay ขั้นต่ำ 500 บาท จึงจะได้รับโบนัส 5%</li>
+                                <li>3. ยอดฝากสำเร็จก่อนเวลา 22:00:00 (GMT+8) ในแต่ละวัน จะสามารถนำมาขอรับโบนัส 5% ได้</li>
+                                <li>4. จะต้องติดต่อเจ้าหน้าที่ผ่านทางแชตสดทุกครั้งเพื่อขอรับโบนัส และต้องไม่นำเงินไปเดิมพันก่อน</li>
+                                <li>5. โบนัสสูงสุด 300 บาท</li>
+                                <li>6. โบนัสเครดิตจะเพิ่มเข้าบัญชีภายใน 24 ชัวโมง</li>
+                                <li>7. สมาชิกสามารถรับโบนัสของโปรโมชั่นนี้ได้เพียงแค่ 1 ครั้งต่อวันเท่านั้น</li>
+                                <li>8. สมาชิกจะต้องทำยอดเทิร์นโอเวอร์ 10 เท่า จึงจะสามารถถอนเงินได้</li>
+                                <li>9. Letou ขอสงวนสิทธิ์ในการแก้ไข เปลี่ยนแปลง ยกเลิกโปรโมชั่นโดยไม่ต้องแจ้งให้ทราบล่วงหน้า</li>
+                                <li>9. โปรโมชั่นนี้สามารถใช่ร่วมกับโปรโมชั่นอื่นได้</li>
+                                <li>9. เงื่อนไขของโปรโมชั่นทั่วไปจะถูกนำมาใช้กับโปรโมชั่นนี้</li>
+                            </ul>
+
+                            <br/>
+
+                            <div>
+                                <img className={classes.bankLogos} src={ images.src + "letou/bonus_images/THBankLogos.png" } alt="BankLogos"/>
+                            </div>
+
+                            <p className={classes.contentHeading}>รายละเอียด ระบบฝากเงินเงินอัตโนมัติ</p>
+
+                            <ul>
+                                <li>1. เพียงท่านสมาชิก มีบัญชีอินเตอร์เน็ตแบงค์ต่างๆ</li>
+                                <li>2. เลือกทำรายการฝากเงิน ที่ฝากเงินอัตโนมัติ Help2Pay</li>
+                                <li>3. หลังทำการเลือกธนาคาร และกรอกจำนวนเงินแล้ว</li>
+                                <li>4. กรอกรายละเอียดอินเตอร์เน็ตแบงค์ ในหน้าต่างและกดตกลง</li>
+                                <li>5. ระบบจะทำการเพิ่มเครดิต ให้ท่านสมาชิกโดยอัตโนมัติ พร้อมสนุกไปกับทุกบริการของ LETOU ได้ทันที</li>
+                            </ul>
+
+                            <br/>
+
+                            <p>
+                                **ข้อควรระวัง**: กรุณาตรวจสอบกับเจ้าหน้าที่ก่อนทุกครั้งในการขอรับโบนัส หากท่านทำการสมัครและขอรับโบนัสในที่สาธารณะ เช่น ที่ทำงาน, ร้านอินเตอร์เน็ต, มือถือ & แท็บเล็ตเครื่องเดียวกัน หรือ หอพัก เนื่องจากทั้งหมดที่กล่าวมาอาจจะทำให้เป็นการรับโบนัสซ้ำซ้อนและถูกริบเงินรางวัลในภายหลังหากมีการตรวจสอบพบ
+                            </p>
+
+                        </div>
+                    </div>
                 </div>
 
                 <div className={classes.footer}>
--- src/letou/components/bonus/th_one_click_register.js
+++ src/letou/components/bonus/th_one_click_register.js
@@ -55,6 +55,14 @@ const styles = {
         lineHeight: 2,
         color: 'grey',
     },
+    contentImages: {
+        width: '100%',
+    },
+    centeredImages: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
 };
 
 class THOneClickRegisterBonus extends React.Component {
@@ -76,7 +84,76 @@ class THOneClickRegisterBonus extends React.Component {
                 <div className={classes.content}>
                     
                     <h3 className={classes.mainHeading}>
+                        One Click Register
+                        <br/>
+                        เจ้าแรกในเอเซีย คลิกเดียวเข้าเล่นได้ทันที!!!!
+                        <br/>
+                        <img src={ images.src + "letou/bonus_images/one_click_register/TopButton.png" } alt="TopButton"/>
                     </h3>
+
+                    <ul>
+                        <li>ปกติเวลาผู้เล่นทั่วไปจะเริ่มเล่นเว็บเลโต้ ผู้เล่นต้องทำการลงทะเบียนก่อน โดยกรอกข้อมูล 4-5 ส่วนดังนี้</li>
+                        <li>• ชื่อผู้ใช้งาน</li>
+                        <li>• รหัสผ่าน</li>
+                        <li>• ยืนยันรหัสผ่าน</li>
+                        <li>• หมายเลขโทรศัทพ์</li>
+                        <li>• รหัสตัวแทน (กรอกเฉพาะในกรณีที่เพื่อนเป็นผู้แนะนำมาเล่น)</li>
+
+
+
+                        <br/>
+                        <img className={classes.contentImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick01.jpg" } alt="THOneClick01"/>
+
+                        <li>ระบบ "One Click" เป็นระบบใหม่ ที่จะอำนวยความสะดวกแก่ผู้เล่น ให้สามารถรับยูสเซอร์และพาสเวิร์ดทันทีภายในคลิกเดียว</li>
+                        <li>โดยที่ไม่ต้องลงทะเบียนเต็มรูปแบบ ใช้งานได้ง่ายๆ ดังนี้ :</li>
+                        <li>1.คลิกที่ปุ่ม [คลิกที่นี่ เพื่อรับยูสทันที]</li>
+
+
+                        <img className={classes.contentImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick02.jpg" } alt="THOneClick02"/>
+
+                        <li>2.ระบบของเว็บเลโต้ จะสร้างยูสเซอร์และพาสเวิร์ดให้แก่ผู้เล่นทันที</li>
+                        <li>•ผู้เล่นสามารถก็อปปี้ชื่อยูสเซอร์ และพาสเวิร์ด แล้วนำไปวางไว้และบันทึกเป็นไฟล์ข้อความได้</li>
+                        <li>•ผู้เล่นสามารถทำการส่งข้อมูล ยูสเซอร์และพาสเวิร์ดไปที่อีเมล์ หรือ SMS ไปที่โทรศัพท์มือถือได้</li>
+                        <li>(เพื่อเป็นการบันทึกไว้ หรือในกรณีที่ผู้เล่นลืมยูสเซอร์และพาสเวิร์ด)</li>
+
+
+                        <img className={classes.contentImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick03.jpg" } alt="THOneClick03"/>
+
+
+                        <li>ผู้เล่นสามารถเลือกส่งยูสเซอร์และพาสเวิร์ดไปที่อีเมล์ได้</li>
+
+                        <img className={classes.contentImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick04.jpg" } alt="THOneClick04"/>
+
+
+                        <li>ผู้เล่นสามารถเลือกส่งยูสเซอร์และพาสเวิร์ดไปที่โทรศัพท์ ผ่าน SMS ได้ (การใส่หมายเลขโทรศัพท์มือถือ ต้องตัด "0" ตัวแรกออก)</li>
+
+
+                        <img className={classes.contentImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick05.jpg" } alt="THOneClick05"/>
+
+
+
+                        <li>หลังจากคลิก "ฝากเพื่อเล่นทันที" ผู้เล่นต้องกรอกข้อมูลชื่อและนามสกุลจริง ก่อนการฝาก (ทำเพียงครั้งแรกครั้งเดียว)</li>
+
+                        <img className={classes.contentImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick06.jpg" } alt="THOneClick06"/>
+
+
+
+                        <li>กรอกชื่อจริง นามสกุลจริง และคลิกที่ [ขั้นต่อไป]</li>
+
+                        <img className={classes.contentImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick07.jpg" } alt="THOneClick07"/>
+
+
+
+                        <li>หลังจากนั้นเป็นขั้นตอนการฝาก สมาชิกสามารถเลือกการฝากได้ดังนี้ : [ฝากผ่านบัญชีธนาคาร], [ฝากอัตโนมัติ (อินเตอร์เน็ตแบงค์)] และ [ฝากผ่าน QR Code] ได้ทันที</li>
+
+
+                    </ul>
+
+                    <img className={classes.centeredImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick08.jpg" } alt="THOneClick08"/>
+                    <img className={classes.centeredImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick09.jpg" } alt="THOneClick09"/>
+                    <img className={classes.centeredImages} src={ images.src + "letou/bonus_images/one_click_register/THOneClick10.jpg" } alt="THOneClick10"/>
+                    <br/>
+                    <img className={classes.centeredImages} src={ images.src + "letou/bonus_images/one_click_register/BottomButton.png" } alt="BottomButton"/>
 
                 </div>
 
