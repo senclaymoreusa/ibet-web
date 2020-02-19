5c107842fae8f9420e65f75148e0cdcdee427e17
Samet <samet@claymoreusa.com>
2020-02-19 13:48:56 -0800

    bug fix

--- src/letou/components/profile/fortune_center/fortune_center.js
+++ src/letou/components/profile/fortune_center/fortune_center.js
@@ -180,9 +180,6 @@ export class FortuneCenter extends Component {
         super(props);
 
         this.state = {
-            urlPath: '',
-            desktopContent: '',
-            desktopTabValue: '',
             userInformationEditMessage: '',
             message: '',
             mainWallet: 0,
@@ -216,9 +213,9 @@ export class FortuneCenter extends Component {
             }
         })
 
-        this.setState({ urlPath: this.props.history.location.pathname });
+        // this.setState({ urlPath: this.props.history.location.pathname });
 
-        this.initializeContent();
+       // this.initializeContent();
     }
 
     initializeContent() {
@@ -416,8 +413,7 @@ export class FortuneCenter extends Component {
     }
 
     render() {
-        const { classes } = this.props;
-        const { desktopContent, desktopTabValue } = this.state;
+        const { classes, subProp } = this.props;
 
         return (
             <div className={classes.root}>
@@ -428,32 +424,32 @@ export class FortuneCenter extends Component {
                         </Grid>
                         <Grid item xs={12} style={{ display: 'flex', flexDirection: 'row', paddingTop: 20 }}>
                             <div className={classes.leftPane}>
-                                <Button className={(desktopTabValue === 'total-assets') ? classes.activeLeftPaneButton : classes.leftPaneButton}
+                                <Button className={(subProp === 'total-assets') ? classes.activeLeftPaneButton : classes.leftPaneButton}
                                     onClick={() => this.props.history.push('/p/fortune-center/total-assets')}>
                                     <AccountBalanceOutlined style={{ marginRight: 8 }} />
                                     {this.getLabel('total-assets')}
                                 </Button>
-                                <Button className={(desktopTabValue === 'deposit') ? classes.activeLeftPaneButton : classes.leftPaneButton}
+                                <Button className={(subProp === 'deposit') ? classes.activeLeftPaneButton : classes.leftPaneButton}
                                     onClick={() => this.props.history.push('/p/fortune-center/deposit')}>
                                     <FlightLandOutlined style={{ marginRight: 8 }} />
                                     {this.getLabel('deposit-label')}
                                 </Button>
-                                <Button className={(desktopTabValue === 'withdraw') ? classes.activeLeftPaneButton : classes.leftPaneButton}
+                                <Button className={(subProp === 'withdraw') ? classes.activeLeftPaneButton : classes.leftPaneButton}
                                     onClick={() => this.props.history.push('/p/fortune-center/withdraw')}>
                                     <FlightTakeoffOutlined style={{ marginRight: 8 }} />
                                     {this.getLabel('withdraw-label')}
                                 </Button>
-                                <Button className={(desktopTabValue === 'transfer') ? classes.activeLeftPaneButton : classes.leftPaneButton}
+                                <Button className={(subProp === 'transfer') ? classes.activeLeftPaneButton : classes.leftPaneButton}
                                     onClick={() => this.props.history.push('/p/fortune-center/transfer')}>
                                     <LoopOutlined style={{ marginRight: 8 }} />
                                     {this.getLabel('title-transfer')}
                                 </Button>
                             </div>
                             <div className={classes.content}>
-                                {desktopContent === 'deposit' && <DepositMain />}
-                                {desktopContent === 'total-assets' && <TotalAssets />}
-                                {desktopContent === 'transfer' && <Transfer />}
-                                {desktopContent === 'withdraw' && <WithdrawMain />}
+                                {subProp === 'deposit' && <DepositMain />}
+                                {subProp === 'total-assets' && <TotalAssets />}
+                                {subProp === 'transfer' && <Transfer />}
+                                {subProp === 'withdraw' && <WithdrawMain />}
                             </div>
                         </Grid>
                     </Grid>
